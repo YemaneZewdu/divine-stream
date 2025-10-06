0b9fc07512ea94fb8f8031fc4f83f68839779147
Yemane <152532702+YemaneInfraTech@users.noreply.github.com>
2025-10-06 14:26:13 +0300

    feat: add connectivity_service.dart to check for connectivity issues

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,6 +6,7 @@ import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:divine_stream/services/playlist_service.dart';
 import 'package:divine_stream/services/google_drive_service.dart';
+import 'package:divine_stream/services/connectivity_service.dart';
 import 'package:divine_stream/ui/views/playlist/playlist_view.dart';
 import 'package:divine_stream/services/audio_player_service.dart';
 import 'package:divine_stream/services/audio_handler_impl_service.dart';
@@ -24,6 +25,7 @@ import 'package:divine_stream/services/audio_handler_impl_service.dart';
     LazySingleton(classType: NavigationService),
     LazySingleton(classType: PlaylistService),
     LazySingleton(classType: GoogleDriveService),
+    LazySingleton(classType: ConnectivityService),
     LazySingleton(classType: AudioPlayerService),
     LazySingleton(classType: AudioHandlerImplService),
 // @stacked-service
